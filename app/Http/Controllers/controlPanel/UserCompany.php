917ef923aa92e2989a65d396eedbf96f3254ccd8
djacobo-DH <djacobo@hiberus.com>
2026-03-19 09:24:13 +0100

    fix: filter user tenant control-panel

--- app/Http/Controllers/controlPanel/UserCompany.php
+++ app/Http/Controllers/controlPanel/UserCompany.php
@@ -3,14 +3,5 @@
 namespace App\Http\Controllers\controlPanel;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 
-class UserCompany extends Controller
-{
-    public function UserControl()
-    {
-        $users = User::all();
-
-        return view('dashboard.features.control-panel.controlPanelApp', compact('users'));
-    }
-}
+class UserCompany extends Controller {}
